949adc66ad7d46edbf1ae8112d849b0438c3bb44
Srinivasa Durai <kaizel73@gmail.com>
2023-03-15 10:18:27 +0530

    first

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from 'react';
 import Chat from './chat';
 
-const socket = io.connect("https://socket-io-project-be.herokuapp.com/");
+const socket = io.connect("http://localhost:4000");
 
 function App() {
 
